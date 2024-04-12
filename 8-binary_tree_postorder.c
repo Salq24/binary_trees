e5307e0d84edc32ceb45f1a7f18f85dd352e29bb
Salq24 <qogheneochuko@gmail.com>
2024-04-12 08:45:54 -0700

    binary

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -6,17 +6,14 @@
  * @tree: pointer to the root node of the tree
  * @func: pointer to the function
 */
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int)) 
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL) 
-    {
-        return;
-    }
-    
-    
-    binary_tree_postorder(tree->left, func);
-
-    binary_tree_postorder(tree->right, func);
-
-    func(tree->n);
+	if (tree == NULL || func == NULL)
+		return;
+	
+	binary_tree_postorder(tree->left, func);
+	
+	binary_tree_postorder(tree->right, func);
+	
+	func(tree->n);
 }
