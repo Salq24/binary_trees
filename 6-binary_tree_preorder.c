5d1116c00727994c1d4dd0e352a1b9cf1a082b93
Salq24 <qogheneochuko@gmail.com>
2024-04-12 08:41:34 -0700

    binary

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -6,16 +6,13 @@
  * @tree: pointer to the root node of the tree
  * @func: pointer to the function
 */
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int)) 
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL) 
-    {
-        return;
-    }
-    
-    func(tree->n);
-    
-    binary_tree_preorder(tree->left, func);
-    
-    binary_tree_preorder(tree->right, func);
+	if (tree == NULL || func == NULL)
+		return;
+
+	func(tree->n);
+
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
