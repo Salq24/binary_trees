78015642ba4b4408de597b05b72ef804dd4b5e8f
Salq24 <qogheneochuko@gmail.com>
2024-04-12 08:49:53 -0700

    binary

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,13 +10,16 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t h_l;
+	size_t h_r;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
 
-	size_t h_l = tree->left ? 1 + binary_tree_height(tree->left): 0;
-	size_t h_r = tree->right ? 1 + binary_tree_height(tree->right): 0;
+	h_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	h_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 	return (h_l > h_r ? h_l : h_r);
 }
